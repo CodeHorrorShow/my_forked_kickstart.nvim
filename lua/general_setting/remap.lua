ec1960d12cbbd7e56a3bc5ab71049f5ebb46a379
CodeHorrorShow <127965775+CodeHorrorShow@users.noreply.github.com>
2024-12-30 19:08:31 +0800

    Update moveline key remap

--- lua/general_setting/remap.lua
+++ lua/general_setting/remap.lua
@@ -4,8 +4,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line down(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line up(n)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move line down(v)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line down(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line up(v)
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
